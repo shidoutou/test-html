4059be7885f5cdcf4b7894ff1e4e14b90efa5d60
tzt <shidoutou11@gmail.com>
2023-11-06 11:40:16 +0900

    1

---
+++ search.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('#searchBtn').click(function() {
+    //alert('元素已被点击！');、
+    // 清空表格当前内容
+    $('#student-tbody').empty();
+    
+    $.ajax({
+      url: 'http://192.168.0.6:3000/students',
+      method: 'GET',
+      success: function(data) {
+        // 循环 students 数组并将数据添加到表格
+        $.each(data, function(index, student) {
+          $('#student-tbody').append(
+            '<tr>' +
+            '<td><input type="checkbox" name="studentCheckbox"></td>' +
+            '<td>' + (index + 1) + '</td>' +
+            '<td>' + student.id + '</td>' +
+            '<td>' + student.name + '</td>' +
+            '<td>' + student.gender + '</td>' +
+            '<td>' + student.class + '</td>' +
+            '<td>' + student.age + '</td>' +
+            '<td>' + student.remarks + '</td>' +
+            '</tr>'
+          );
+        });
+      },
+      error: function(error) {
+        console.error('Error:', error);
+      }
+    });
+  });
+});
